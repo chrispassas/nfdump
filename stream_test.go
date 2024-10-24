efe221595f206a2aff437f3d1a80b1fad590a05b
Chris Passas <chrispassas@users.noreply.github.com>
2024-10-24 10:33:12 -0400

    - rename test file - add stream test for detecting corrupt file

--- stream_test.go
+++ stream_test.go
@@ -8,6 +8,38 @@ import (
 	"testing"
 )
 
+func TestCorruptStreamReader(t *testing.T) {
+
+	var fileName = "testdata/nfcapd-corrupt"
+	var data []byte
+	var err error
+	if data, err = ioutil.ReadFile(fileName); err != nil {
+		t.Error(err)
+	}
+
+	var reader = bytes.NewReader(data)
+	var nfs *NFStream
+	nfs, err = StreamReader(reader)
+	if err != nil {
+		t.Errorf("StreamReader error:%#+v", err)
+	}
+
+	for {
+		if _, err = nfs.Row(); err == io.EOF {
+			goto Stop
+		} else if err != nil && err.Error() != "Corrupt file, bad record size:0" {
+			t.Errorf("nfs.Row() error:%v", err)
+			goto Stop
+		} else if err != nil && err.Error() == "Corrupt file, bad record size:0" {
+			// Found expected error
+			return
+		}
+
+	}
+Stop:
+	t.Errorf("Failed to detected corrupt file")
+}
+
 func TestStreamReader(t *testing.T) {
 
 	var data []byte
